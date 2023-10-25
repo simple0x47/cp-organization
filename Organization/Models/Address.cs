db8bdc2240e7a1d6b90ef8673071e898bc71b6a8
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-25 22:02:57 +0200

    + initial commit

---
+++ Organization/Models/Address.cs
@@ -0,0 +1,12 @@
+namespace Organization.Models;
+
+public struct Address
+{
+    public string Country;
+    public string Province;
+    public string City;
+    public string Street;
+    public string Number;
+    public string Additional;
+    public string PostalCode;
+}
